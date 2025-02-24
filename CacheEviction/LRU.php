a610ac88382832590a7428ce3d37d5ba188b9a45
madi1990 <chlyrobert@hotmail.com>
2025-02-24 15:56:48 +1000

    Update LRU algorithm

--- CacheEviction/LRU.php
+++ CacheEviction/LRU.php
@@ -7,11 +7,35 @@ class LRU
     public function __construct(private int $capacity, private SplDoublyLinkedList $cacheList){
     }
 
-    public function addElement(string $element): void {
+    private function addElement(string $element): void {
         if ($this->cacheList->count() >= $this->capacity) {
             $this->cacheList->shift();
         }
         $this->cacheList->push($element);
+    }
+
+    private function isElementExisting(string $element, bool $removeUponFound = false): bool {
+        $this->cacheList->rewind();
+        while ($this->cacheList->valid()) {
+            if ($this->cacheList->current() === $element) {
+                if ($removeUponFound) {
+                    $this->cacheList->offsetUnset($this->cacheList->key());
+                }
+                return true;
+            }
+            $this->cacheList->next();
+        }
+        return false;
+    }
+
+    public function accessElement(string $element): void {
+        if ($this->isElementExisting($element, true)) {
+            // Cache hit
+            $this->addElement($element);
+        } else {
+            // Cache miss
+            $this->addElement($element);
+        }
     }
 
     public function outputCacheList(): void {
@@ -23,11 +47,11 @@ class LRU
 }
 
 $lru = new LRU(3, new SplDoublyLinkedList());
-$lru->addElement('A');
-$lru->addElement('B');
-$lru->addElement('C');
+$lru->accessElement('A');
+$lru->accessElement('B');
+$lru->accessElement('C');
 $lru->outputCacheList();
-$lru->addElement('A');
+$lru->accessElement('B');
 $lru->outputCacheList();
-$lru->addElement('D');
+$lru->accessElement('D');
 $lru->outputCacheList();
